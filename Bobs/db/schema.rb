1484cbd374b4ae2e7b6f204a5b82262f2b09c21d
Leslie <quickl14@students.ecu.edu>
2017-03-24 00:02:22 +0000

    Second Commit

--- Bobs/db/schema.rb
+++ Bobs/db/schema.rb
@@ -11,17 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323141044) do
+ActiveRecord::Schema.define(version: 20170313204531) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",              limit: 255
-    t.string   "password",           limit: 255
-    t.string   "username",           limit: 255
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.string   "encrypted_password", limit: 128
+    t.string   "email",              limit: 255, null: false
+    t.string   "encrypted_password", limit: 128, null: false
     t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128
+    t.string   "remember_token",     limit: 128, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
