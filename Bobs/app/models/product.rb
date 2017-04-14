895fbb232e5831e9314b04d5d5df07da6a9c7678
Leslie <quickl14@students.ecu.edu>
2017-04-14 14:43:38 +0000

    few css changes

--- Bobs/app/models/product.rb
+++ Bobs/app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ActiveRecord::Base
-  has_many :order_items
-
+  has_many :order_items, dependent: :destroy
   default_scope { where(active: true) }
 end
--- Bobs/db/seeds.rb
+++ Bobs/db/seeds.rb
@@ -4,16 +4,11 @@
 # Examples:
 #
 #   cities = users.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-Product.delete_all
-Product.create! id: 1, name: "Banana", price: 0.49, active: true
-Product.create! id: 2, name: "Apple", price: 0.29, active: true
-Product.create! id: 3, name: "Carton of Strawberries", price: 1.99, active: true
 
-OrderStatus.delete_all
-OrderStatus.create! id: 1, name: "In Progress"
-OrderStatus.create! id: 2, name: "Placed"
-OrderStatus.create! id: 3, name: "Shipped"
-OrderStatus.create! id: 4, name: "Cancelled"
+
+
+
 
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+AdminUser.delete_all
 AdminUser.create!(email: 'eldutche@gmail.com', password: 'testing', password_confirmation: 'testing')
